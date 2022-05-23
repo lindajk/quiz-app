295db35acd37601a734fa7e5d39769eb5ed98b21
Linda Kuang <linda.jk@gmx.de>
2022-05-23 10:33:51 +0200

    Update bookmark.js

--- bookmark.js
+++ bookmark.js
@@ -4,4 +4,13 @@ const bookmarkIcon = document.querySelector('[data-js="qc__bookmark"]');
 bookmarkIcon.addEventListener('click', () => {
   bookmarkIcon.classList.toggle('qc__bookmark-filled');
 });
-console.log(bookmarkIcon);
+
+const showanswerButton = document.querySelector(
+  '[data-js="qc__showanswer-button"]'
+);
+const answerText = document.querySelector('[data-js="qc__answer"]');
+
+showanswerButton.addEventListener('click', () => {
+  answerText.classList.toggle('qc__answer-active');
+});
+console.log();
